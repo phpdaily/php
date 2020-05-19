f9e28095887d75c235d62289647bd9da5d73422b
Jérémy DECOOL <contact@jdecool.fr>
2020-05-19 22:19:52 +0200

    Update Docker Hub description script

--- push-hub-description.sh
+++ push-hub-description.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
-TOKEN=$(curl -s -X POST \
-    -H "Content-Type: application/json" \
-    -d '{"username": "'"$DOCKER_USERNAME"'", "password": "'"$DOCKER_PASSWORD"'"}' \
-    https://hub.docker.com/v2/users/login/ | jq -r .token)
-
-CODE=$(jq -n --arg msg "$(<README.md)" \
-    '{"registry":"registry-1.docker.io","full_description": $msg }' | \
-        curl -s -o /dev/null -L -w "%{http_code}" \
-           https://cloud.docker.com/v2/repositories/"${DOCKER_IMAGE}"/ \
-           -d @- -X PATCH \
-           -H "Content-Type: application/json" \
-           -H "Authorization: JWT ${TOKEN}")
-
-if [[ "${CODE}" = "200" ]]; then
-    printf "Successfully pushed README to Docker Hub\n"
-else
-    printf "Unable to push README to Docker Hub, response code: %s\n" "${CODE}"
-    exit 1
-fi
+docker run -v $WORKSPACE:/workspace \
+  -e DOCKERHUB_USERNAME="$DOCKER_USERNAME" \
+  -e DOCKERHUB_PASSWORD="$DOCKER_PASSWORD" \
+  -e DOCKERHUB_REPOSITORY="${DOCKER_IMAGE}" \
+  -e README_FILEPATH="/workspace/README.md" \
+  peterevans/dockerhub-description:2.1
