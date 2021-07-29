b92d7e12ca9a5cdb9c968d4aea16d962ec6ece92
Jérémy DECOOL <contact@jdecool.fr>
2021-07-29 20:35:49 +0200

    Upgrade 7.4.22 -> 7.4.23

--- jenkins_build.groovy
+++ jenkins_build.groovy
@@ -95,7 +95,7 @@ generateJob('8.0-dev/buster/cli/', "$version80-dev-cli-buster")
 generateJob('8.0-dev/buster/fpm/', "$version80-dev-fpm-buster")
 generateJob('8.0-dev/buster/zts/', "$version80-dev-zts-buster")
 
-def version74 = "7.4.22"
+def version74 = "7.4.23"
 generateJob('7.4-dev/alpine3.12/cli/', '7.4-dev')
 generateJob('7.4-dev/alpine3.12/cli/', "7.4-dev-alpine3.12")
 generateJob('7.4-dev/alpine3.12/cli/', "$version74-dev-cli-alpine3.12")
